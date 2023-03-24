2888537e5de1801a2509e4a6b28cc33c9919ad24
HoangcoderIkci <128100787+HoangcoderIkci@users.noreply.github.com>
2023-03-24 23:34:57 +0300

    Update main.py

--- inver_matrix/main.py
+++ inver_matrix/main.py
@@ -1,31 +1,30 @@
 import numpy as np
 
-matrix = np.random.random([5,6])
+#matrix = np.random.random([2,6])
+matrix = np.array([[0,0,0,1,0,0,1],
+                   [0,0,0,1,0,1,2]])
  
 vec_hs = []
 rank = 0
 check = True
 i,j = 0,0
 row,col = matrix.shape
-t = min(row,col)
-while i < t and j < t:
+t = min(row,col) 
+while rank < t and j < col:
     check = False
     for i in range(rank,row):
         if matrix[i,j]!=0:
             check = True
-            vec_hs.append(i)
+            vec_hs.append(j)
             break
     if check:
-        temp = matrix[i]
-        matrix[i] = matrix[rank]
-        matrix[rank] = temp
+        matrix[[i,rank]] = matrix[[rank,i]]
         
-        matrix[rank] /= matrix[rank][j]
+        matrix[rank] = matrix[rank] / matrix[rank][j]
         
         for k in range(rank+1,row):  #for(int i = rang+1;i<row;i++)
             matrix[k]-= matrix[rank] * matrix[k][j]
         rank+=1
-    i+=1 
     j+=1
 
 i=0
@@ -35,6 +34,3 @@ for k in vec_hs:
         matrix[j]-= matrix[i] * matrix[j][k]
     i+=1
 print(matrix)
-        
-    
-        
